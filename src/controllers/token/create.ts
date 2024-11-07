397880c5cd3112dcd88b8873db57309e6d7f48ae
Caio Henrique Alves <50040463+CaioHAlves@users.noreply.github.com>
2024-11-07 15:43:54 -0300

    ajuste tipagem

--- src/controllers/token/create.ts
+++ src/controllers/token/create.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 export class GenerateToken {
   static async create(req: Request, res: Response) {
 
-    const { expires, secret } = req.query
+    const { expires, secret }: { expires?: string, secret?: string } = req.query;
 
     try {
       const token = jwt.sign(req.body, String(secret) || process.env.JWT!, {
